04eb741069052e989cbdc9ab81259363f9101875
jaysoncano <cano.jayson10@gmail.com>
2024-09-05 09:46:04 +0800

    Fix: main component files - css issue conflict

--- src/components/main/menu.tsx
+++ src/components/main/menu.tsx
@@ -1,96 +1,133 @@
-"use client"
+"use client";
 
-import { updatePostArchivedAtAction, updatePostDeletedAtAction, updatePostFavoritedAtAction } from '@/actions/post.action';
-import { useRouter } from 'next/navigation';
-import React, { Dispatch, SetStateAction, useTransition, MouseEvent } from 'react'
-import { FaRegStar } from 'react-icons/fa6';
-import { FiArchive, FiTrash } from 'react-icons/fi';
-import { GoStarFill } from 'react-icons/go';
-import { toast } from 'sonner';
+import {
+  updatePostArchivedAtAction,
+  updatePostDeletedAtAction,
+  updatePostFavoritedAtAction,
+} from "@/actions/post.action";
+import { useRouter } from "next/navigation";
+import React, {
+  Dispatch,
+  SetStateAction,
+  useTransition,
+  MouseEvent,
+} from "react";
+import { FaRegStar } from "react-icons/fa6";
+import { FiArchive, FiTrash } from "react-icons/fi";
+import { GoStarFill } from "react-icons/go";
+import { toast } from "sonner";
 
 type Props = {
-    setOpenMenu: Dispatch<SetStateAction<boolean>>;
-    postId: string | null;
-    favoritedAt: Date | null;
-    archivedAt: Date | null;
-}
+  setOpenMenu: Dispatch<SetStateAction<boolean>>;
+  postId: string | null;
+  favoritedAt: Date | null;
+  archivedAt: Date | null;
+};
 
 const Menu = ({ setOpenMenu, postId, favoritedAt, archivedAt }: Props) => {
-    const router = useRouter();
-    const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
-    const closeMenu = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
-        e.stopPropagation();
-        setOpenMenu(false);
+  const closeMenu = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
+    e.stopPropagation();
+    setOpenMenu(false);
+  };
+
+  const deleted = async () => {
+    if (!postId || isPending) return null;
+
+    try {
+      startTransition(() => {
+        updatePostDeletedAtAction({ postId, path: window.location.pathname });
+      });
+    } catch (error) {
+      console.info("[ERROR_DELETED]", error);
+
+      toast.error("Something went wrong.");
     }
+  };
 
-    const deleted = async () => {
-        if (!postId || isPending) return null;
+  const favorited = async () => {
+    if (!postId || isPending) return null;
 
-        try {
-            startTransition(() => {
-                updatePostDeletedAtAction({ postId, path: window.location.pathname });
-            });
-        } catch (error) {
-            console.info('[ERROR_DELETED]', error);
+    try {
+      startTransition(() => {
+        updatePostFavoritedAtAction({ postId, path: window.location.pathname });
+      });
+    } catch (error) {
+      console.info("[ERROR_FAVORITED]", error);
 
-            toast.error("Something went wrong.");
+      toast.error("Something went wrong.");
+    }
+  };
+
+  const archived = async () => {
+    if (!postId || isPending) return null;
+
+    try {
+      startTransition(async () => {
+        const res = await updatePostArchivedAtAction({
+          postId,
+          path: window.location.pathname,
+        });
+
+        if (res.message === "Post unarchived successfully." && res.data) {
+          router.push(
+            `/?folderId=${res.data.folderId}&postId=${res.data.postId}`
+          );
         }
-    }
 
-    const favorited = async () => {
-        if (!postId || isPending) return null;
-
-        try {
-            startTransition(() => {
-                updatePostFavoritedAtAction({ postId, path: window.location.pathname });
-            });
-        } catch (error) {
-            console.info('[ERROR_FAVORITED]', error);
-
-            toast.error("Something went wrong.");
+        if (res.message === "Post archived successfully." && res.data) {
+          router.push(`/?category=archived-notes&postId=${res.data.postId}`);
         }
+      });
+    } catch (error) {
+      console.info("[ERROR_ARCHIVED]", error);
+
+      toast.error("Something went wrong.");
     }
+  };
 
-    const archived = async () => {
-        if (!postId || isPending) return null;
+  return (
+    <div
+      onClick={(e) => closeMenu(e)}
+      className="absolute top-[44px] right-0 bg-[#333333] p-4 rounded-[6px] text-white w-[52px] flex flex-col gap-y-5"
+    >
+      <button
+        onClick={favorited}
+        disabled={isPending}
+        className="flex items-center gap-x-4"
+      >
+        {favoritedAt ? (
+          <GoStarFill className="w-5 h-5" />
+        ) : (
+          <FaRegStar className="w-5 h-5" />
+        )}
+        <h5 className="font-sans font-normal text-base">
+          {favoritedAt ? "Unfavorited" : "Add to favorites"}
+        </h5>
+      </button>
+      <button
+        className="flex items-center gap-x-4"
+        onClick={archived}
+        disabled={isPending}
+      >
+        <FiArchive className="w-5 h-5" />
+        <h5 className="font-sans font-normal text-base">
+          {archivedAt ? "Unarchive" : "Add to archive"}
+        </h5>
+      </button>
+      <div className="h-1 w-full bg-white/5" />
+      <button
+        className="flex items-center gap-x-4"
+        disabled={isPending}
+        onClick={deleted}
+      >
+        <FiTrash className="w-5 h-5" />
+        <h5 className="font-sans font-normal text-base">Delete</h5>
+      </button>
+    </div>
+  );
+};
 
-        try {
-            startTransition(async () => {
-                const res = await updatePostArchivedAtAction({ postId, path: window.location.pathname });
-
-                if (res.message === "Post unarchived successfully." && res.data) {
-                    router.push(`/?folderId=${res.data.folderId}&postId=${res.data.postId}`);
-                }
-
-                if (res.message === "Post archived successfully." && res.data) {
-                    router.push(`/?category=archived-notes&postId=${res.data.postId}`);
-                }
-            });
-        } catch (error) {
-            console.info('[ERROR_ARCHIVED]', error);
-
-            toast.error("Something went wrong.");
-        }
-    }
-
-    return (
-        <div onClick={e => closeMenu(e)} className="absolute top-44 right-0 bg-[#333333] p-15 rounded-6 text-white w-202 flex flex-col gap-y-20">
-            <button onClick={favorited} disabled={isPending} className="flex items-center gap-x-15">
-                {favoritedAt ? <GoStarFill className="w-20 h-20" /> : <FaRegStar className="w-20 h-20" />}
-                <h5 className="font-sans font-normal text-16">{favoritedAt ? "Unfavorited" : "Add to favorites"}</h5>
-            </button>
-            <button className="flex items-center gap-x-15" onClick={archived} disabled={isPending}>
-                <FiArchive className="w-20 h-20" />
-                <h5 className="font-sans font-normal text-16">{archivedAt ? "Unarchive" : "Add to archive"}</h5>
-            </button>
-            <div className="h-1 w-full bg-white/5" />
-            <button className="flex items-center gap-x-15" disabled={isPending} onClick={deleted}>
-                <FiTrash className="w-20 h-20" />
-                <h5 className="font-sans font-normal text-16">Delete</h5>
-            </button>
-        </div>
-    )
-}
-
-export default Menu
+export default Menu;
--- src/components/main/restore-deleted.tsx
+++ src/components/main/restore-deleted.tsx
@@ -1,71 +1,99 @@
-import { deletePostPermanentAction, updatePostDeletedAtAction } from '@/actions/post.action';
-import { useRouter, useSearchParams } from 'next/navigation';
-import React, { useTransition } from 'react'
+import {
+  deletePostPermanentAction,
+  updatePostDeletedAtAction,
+} from "@/actions/post.action";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useTransition } from "react";
 import { MdRestore } from "react-icons/md";
-import { toast } from 'sonner';
+import { toast } from "sonner";
 
 type Props = {
-    postId: string;
-}
+  postId: string;
+};
 
 const RestoreDeleted = ({ postId }: Props) => {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const folderId = searchParams.get("folderId");
-    const category = searchParams.get("category");
-    const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const folderId = searchParams.get("folderId");
+  const category = searchParams.get("category");
+  const [isPending, startTransition] = useTransition();
 
-    const restoreAction = () => {
-        if (!postId || isPending) return null;
+  const restoreAction = () => {
+    if (!postId || isPending) return null;
 
-        try {
-            startTransition(async () => {
-                const res = await updatePostDeletedAtAction({ postId, path: window.location.pathname });
+    try {
+      startTransition(async () => {
+        const res = await updatePostDeletedAtAction({
+          postId,
+          path: window.location.pathname,
+        });
 
-                if (res.message === "Post undeleted successfully." && res.data) {
-                    router.push(`/?folderId=${res.data.folderId}&postId=${res.data.postId}`);
-                }
-            });
-        } catch (error) {
-            console.info('[ERROR_RESTORE_ACTION]', error);
-
-            toast('Something went wrong.');
+        if (res.message === "Post undeleted successfully." && res.data) {
+          router.push(
+            `/?folderId=${res.data.folderId}&postId=${res.data.postId}`
+          );
         }
+      });
+    } catch (error) {
+      console.info("[ERROR_RESTORE_ACTION]", error);
+
+      toast("Something went wrong.");
     }
+  };
 
-    const deletePermanent = () => {
-        if (!postId || isPending) return null;
+  const deletePermanent = () => {
+    if (!postId || isPending) return null;
 
-        try {
-            startTransition(async () => {
-                await deletePostPermanentAction({ postId, path: window.location.pathname });
+    try {
+      startTransition(async () => {
+        await deletePostPermanentAction({
+          postId,
+          path: window.location.pathname,
+        });
 
-                if (folderId && postId) {
-                    router.push(`/?folderId=${folderId}&postId=${postId}`)
-                } else if (category) {
-                    router.push(`/?category=${category}`)
-                }
-            });
-        } catch (error) {
-            console.info('[ERROR_DELETE_PERMANENT]', error);
-
-            toast('Something went wrong.');
+        if (folderId && postId) {
+          router.push(`/?folderId=${folderId}&postId=${postId}`);
+        } else if (category) {
+          router.push(`/?category=${category}`);
         }
-    }
+      });
+    } catch (error) {
+      console.info("[ERROR_DELETE_PERMANENT]", error);
 
-    return (
-        <div className="flex items-center justify-center h-full">
-            <div className="flex flex-col items-center text-center gap-y-10">
-                <MdRestore className="w-80 h-80 text-white" />
-                <h1 className="font-sans font-semibold text-28 text-white">Restore</h1>
-                <p className="font-sans font-normal text-16 text-white/60">Don't want to lose this note? It's not too late! Just click the 'Restore'<br /> button and it will be added back to your list. It's that simple..</p>
-                <div className="flex justify-center gap-x-10">
-                    <button className="btn bg-[#312EB5] hover:bg-[#312EB5]/90 disabled:bg=[#312EB5]/60 !w-fit text-white" disabled={isPending} onClick={restoreAction}>Restore</button>
-                    <button className="btn btn-danger" disabled={isPending} onClick={deletePermanent}>Delete</button>
-                </div>
-            </div>
+      toast("Something went wrong.");
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center h-full">
+      <div className="flex flex-col items-center text-center gap-y-3">
+        <MdRestore className="w-[80px] h-[80px] text-white" />
+        <h1 className="font-sans font-semibold text-3xl text-white">Restore</h1>
+        <p className="font-sans font-normal text-base text-white/60">
+          Don't want to lose this note? It's not too late! Just click the
+          'Restore'
+          <br /> button and it will be added back to your list. It's that
+          simple..
+        </p>
+        <div className="flex justify-center gap-x-10">
+          <button
+            className="btn bg-[#312EB5] hover:bg-[#312EB5]/90 disabled:bg=[#312EB5]/60 !w-fit text-white"
+            disabled={isPending}
+            onClick={restoreAction}
+          >
+            Restore
+          </button>
+          <button
+            className="btn btn-danger"
+            disabled={isPending}
+            onClick={deletePermanent}
+          >
+            Delete
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default RestoreDeleted
+export default RestoreDeleted;
