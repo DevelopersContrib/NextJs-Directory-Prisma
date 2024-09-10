7b113fc959972a2d847f87842d265966931dbbc7
jaysoncano <cano.jayson10@gmail.com>
2024-09-10 12:30:23 +0800

    Fix: api charge route

--- src/app/api/charge/route.tsx
+++ src/app/api/charge/route.tsx
@@ -1,12 +1,12 @@
 //https://sultanoveli.medium.com/how-to-add-stripe-payments-to-your-next-js-app-d1cfced7c8a5
-import { NextResponse, NextRequest } from 'next/server';
-import Stripe from 'stripe';
-import { getServerSession } from 'next-auth/next';
+import { NextResponse, NextRequest } from "next/server";
+import Stripe from "stripe";
+import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/lib/utils/auth-options";
-import axios from 'axios';
+import axios from "axios";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
+  apiVersion: "2024-06-20",
   typescript: true,
 });
 
@@ -20,7 +20,7 @@ export const POST = async (req: Request) => {
     try {
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
-        currency: 'USD',
+        currency: "USD",
       });
 
       const data = {
@@ -28,16 +28,17 @@ export const POST = async (req: Request) => {
         amount: amount,
         price: amount,
         client_secret: paymentIntent.client_secret,
-        return_url: process.env.BASE_URL + '/pricing',
+        return_url: process.env.BASE_URL + "/pricing",
       };
 
       // call payment api...
-      const apiUrl = process.env.API_URL + '/payment/charge?api_key=' + process.env.API_KEY;
+      const apiUrl =
+        process.env.API_URL + "/payment/charge?api_key=" + process.env.API_KEY;
 
       const res = await axios.post(apiUrl, data, {
         headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + session?.token,
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + session?.user?.email,
         },
       });
 
