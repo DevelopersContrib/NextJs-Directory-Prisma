0974da88b39d5234baab03eac5b816daa16de63f
Ronan <row_none@yahoo.com>
2024-10-09 19:16:16 -0700

    update user api

---
+++ src/app/api/user/update/route.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const POST = async (req: Request) => {
+  const data = await req.json();
+    
+    if(data.id){
+        try {
+            const user = await prisma.user.update({
+                where: {
+                    id: data.id,
+                },
+                data: {
+                  name:data.name,
+                  password:data.password,
+                },
+              });
+
+            return new Response(JSON.stringify({id:data.id}), { status: 200 });
+        } catch (error) {
+            return new Response(JSON.stringify({ message: 'Error updating user info' }), { status: 200 });  
+        }
+    }    
+    return new Response(JSON.stringify({ message: 'User not found' }), { status: 200 });    
+};
