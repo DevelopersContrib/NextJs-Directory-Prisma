2f7ec836565c179e7bba812539c3063c4e17e0a9
jaysoncano <cano.jayson10@gmail.com>
2024-10-10 09:33:48 +0800

    Fix: text domain color

--- src/components/includes/Logo.jsx
+++ src/components/includes/Logo.jsx
@@ -1,20 +1,22 @@
-import Image from 'next/image'
-import Navbar from 'react-bootstrap/Navbar';
+import Image from "next/image";
+import Navbar from "react-bootstrap/Navbar";
 
-export default function Logo({domain,logo}) {
-  if(logo!=null && logo!=''){
+export default function Logo({ domain, logo }) {
+  if (logo != null && logo != "") {
     return (
-        <Image 
-              src={logo}
-              width={120}
-              height={120}
-              alt=""
-              className='d-inline-flex img-fluid mb-3 tw-pr-3'
-            />
-    )
-  }else{
-    return(
-        <Navbar.Brand href="#home">{domain}</Navbar.Brand>
-    )
+      <Image
+        src={logo}
+        width={120}
+        height={120}
+        alt=""
+        className="d-inline-flex img-fluid mb-3 tw-pr-3"
+      />
+    );
+  } else {
+    return (
+      <Navbar.Brand href="#home" className="text-white">
+        {domain}
+      </Navbar.Brand>
+    );
   }
 }
